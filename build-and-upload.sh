485ed1692ebca9426f7b08f26c104f1275963c70
Ben Kennish <ben@kennish.net>
2014-09-09 10:56:14 +0100

    rsync needs -u

--- build-and-upload.sh
+++ build-and-upload.sh
@@ -11,6 +11,6 @@ echo Building tldr.xpi and tldr.update.rdf...
 cfx xpi --update-link https://www.bennish.net/files/tldr.xpi --update-url https://www.bennish.net/files/tldr.update.rdf
 
 echo Rsyncing files to www.bennish.net...
-rsync -vu tldr.* ben@kennish.net:bennish/files/
+rsync -vau tldr.* ben@kennish.net:bennish/files/
 
 echo All done
