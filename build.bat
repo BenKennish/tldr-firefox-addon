d88ac1ecaebd0dc6ce673e4008ab995b26de16c8
Ben Kennish <ben@kennish.net>
2014-09-12 09:25:47 +0100

    Build and upload scripts include mobile version

--- build.bat
+++ build.bat
@@ -1,6 +1,9 @@
 @echo off
-rem ---- A batch file to build tldr.xp and tldr.update.rdf
+rem ---- A batch file to build tldr.xpi and tldr.update.rdf
 
-echo Building tldr.xpi and tldr.update.rdf...
-cfx xpi --update-link https://www.bennish.net/files/tldr.xpi --update-url https://www.bennish.net/files/tldr.update.rdf
-echo Done
+echo Building .xpi and .update.rdf files...
+echo Mobile:
+call cfx xpi --force-mobile --output-file=tldr-mobile.xpi --update-link https://www.bennish.net/files/tldr-mobile.xpi --update-url https://www.bennish.net/files/tldr-mobile.update.rdf
+move tldr.update.rdf tldr-mobile.update.rdf
+echo Desktop:
+call cfx xpi --update-link https://www.bennish.net/files/tldr.xpi --update-url https://www.bennish.net/files/tldr.update.rdf
