50dd8bd3c544778eaaf8084c43050493e59e759a
Ben Kennish <ben@kennish.net>
2014-09-28 09:11:25 +0100

    Moved unused panel .js files. Build script builds AMO version. Added screenshots. Better description

--- build.bat
+++ build.bat
@@ -2,8 +2,12 @@
 rem ---- A batch file to build tldr.xpi and tldr.update.rdf
 
 echo Building .xpi and .update.rdf files...
-echo Mobile...
-call cfx xpi --force-mobile --output-file=tldr-mobile.xpi --update-link https://www.bennish.net/files/tldr-mobile.xpi --update-url https://www.bennish.net/files/tldr-mobile.update.rdf
-move tldr.update.rdf tldr-mobile.update.rdf
-echo Desktop...
+
+echo Building mobile version (skipping, disabled)
+rem call cfx xpi --force-mobile --output-file=tldr-mobile.xpi --update-link https://www.bennish.net/files/tldr-mobile.xpi --update-url https://www.bennish.net/files/tldr-mobile.update.rdf
+rem move tldr.update.rdf tldr-mobile.update.rdf
+
+echo Building non-AMO desktop version...
 call cfx xpi --update-link https://www.bennish.net/files/tldr.xpi --update-url https://www.bennish.net/files/tldr.update.rdf
+echo Building AMO desktop version...
+call cfx xpi --output-file=tldr-amo.xpi
