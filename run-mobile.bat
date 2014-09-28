9904fecd1a15f23f8eef69f51aad90642d33b90f
Ben Kennish <ben@kennish.net>
2014-09-28 07:59:02 +0100

    Uses newly renamed HTTP headers. Shuts down cleanly

--- run-mobile.bat
+++ run-mobile.bat
@@ -1,4 +1,5 @@
 @echo off
 
 echo Running cfx for attached Android device...
-cfx run -v --mobile-app firefox -a fennec-on-device -b "C:\Users\Ben\AppData\Local\Android\android-sdk\platform-tools\adb.exe" --force-mobile < nul
+rem (values for mobile-app include fennec (Nightly), fennec_aurora (Aurora), firefox_beta (Beta), and firefox (Release) 
+cfx run -v --mobile-app fennec -a fennec-on-device -b "C:\Users\Ben\AppData\Local\Android\android-sdk\platform-tools\adb.exe" --force-mobile < nul
